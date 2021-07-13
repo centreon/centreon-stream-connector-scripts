11b26d374536dec0ef2f58a52c8e172e72428c0b
tanguyvda <tcharles@centreon.com>
2021-07-13 18:17:48 +0200

    add logging and comments for flush module

--- modules/centreon-stream-connectors-lib/sc_flush.lua
+++ modules/centreon-stream-connectors-lib/sc_flush.lua
@@ -4,7 +4,6 @@
 -- Module that handles data queue for stream connectors
 -- @module sc_flush
 -- @alias sc_flush
-
 local sc_flush = {}
 
 local sc_logger = require("centreon-stream-connectors-lib.sc_logger")
@@ -17,6 +16,7 @@ local ScFlush = {}
 function sc_flush.new(params, logger)
   local self = {}
   
+  -- create a default logger if it is not provided
   self.sc_logger = logger
   if not self.sc_logger then 
     self.sc_logger = sc_logger.new()
@@ -28,38 +28,35 @@ function sc_flush.new(params, logger)
   local categories = self.params.bbdo.categories
   local elements = self.params.bbdo.elements
   
+  -- link queue flush info to their respective categories and elements
   self.queue = {
     [categories.neb] = {
       [elements.acknowledgement] = {
         flush_date = os_time,
-        send_data = false,
         events = {}
       },
       [elements.downtime] = {
         flush_date = os_time,
-        send_data = false,
         events = {}
       },
       [elements.host_status] = {
         flush_date = os_time,
-        send_data = false,
         events = {}
       }, 
       [elements.service_status] = {
         flush_date = os_time,
-        send_data = false,
         events = {}
       }
     },
     [categories.bam] = {
       [elements.ba_status] = {
         flush_date = os_time,
-        send_data = false,
         events = {}
       }
     }
   }
 
+  -- link flush methods to their corresponding categories and elements
   self.flush = {
     [categories.neb] = {
       [elements.acknowledgement] = function (send_method) return self:flush_ack(send_method) end,
@@ -76,20 +73,25 @@ function sc_flush.new(params, logger)
   return self
 end
 
-
+--- flush_all_queues: tries to flush all kind of queues
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_all_queues(send_method)
+  self.sc_logger:debug("[sc_flush:flush_all_queues]: Starting to flush all queues")
   self:flush_ack(send_method)
   self:flush_dt(send_method)
   self:flush_host(send_method)
   self:flush_service(send_method)
   self:flush_ba(send_method)
+  self.sc_logger:debug("[sc_flush:flush_all_queues]: All queues have been flushed")
 end
 
-
+--- flush_host: tries to flush the host queue
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_host(send_method)
   local category = self.params.bbdo_info.host_status.category
   local element = self.params.bbdo_info.host_status.element
 
+  -- flush the queue if possible
   if self.flush_queue(send_method, category, element) then
     self.reset_queue(category, element)
     return true
@@ -98,10 +100,13 @@ function ScFlush:flush_host(send_method)
   end
 end
 
+--- flush_host: tries to flush the service queue
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_service(send_method)
   local category = self.params.bbdo_info.service_status.category
   local element = self.params.bbdo_info.service_status.element
 
+  -- flush the queue if possible
   if self.flush_queue(send_method, category, element) then
     self.reset_queue(category, element)
     return true
@@ -110,10 +115,13 @@ function ScFlush:flush_service(send_method)
   end
 end
 
+--- flush_host: tries to flush the acknowledgement queue
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_ack(send_method)
   local category = self.params.bbdo_info.acknowledgement.category
   local element = self.params.bbdo_info.acknowledgement.element
 
+  -- flush the queue if possible
   if self.flush_queue(send_method, category, element) then
     self.reset_queue(category, element)
     return true
@@ -122,10 +130,13 @@ function ScFlush:flush_ack(send_method)
   end
 end
 
+--- flush_host: tries to flush the downtime queue
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_dt(send_method)
   local category = self.params.bbdo_info.downtime.category
   local element = self.params.bbdo_info.downtime.element
 
+  -- flush the queue if possible
   if self.flush_queue(send_method, category, element) then
     self.reset_queue(category, element)
     return true
@@ -134,10 +145,13 @@ function ScFlush:flush_dt(send_method)
   end
 end
 
+--- flush_host: tries to flush the BA queue
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
 function ScFlush:flush_ba(send_method)
   local category = self.params.bbdo_info.ba_status.category
   local element = self.params.bbdo_info.ba_status.element
 
+  -- flush the queue if possible
   if self.flush_queue(send_method, category, element) then
     self.reset_queue(category, element)
     return true
@@ -146,16 +160,25 @@ function ScFlush:flush_ba(send_method)
   end
 end
 
+--- flush_queue: flush a queue if requirements are met
+-- @param send_method (function) the function from the stream connector that will send the data to the wanted tool
+-- @param category (number) the category related to the queue
+-- @param element (number) the element related to the queue
+-- @return true|false (boolean) true if the queue is not flushed and true or false depending the send_method result 
 function ScFlush:flush_queue(send_method, category, element)
-  -- no events stored in the queue
+  -- no events are stored in the queue
   if (#self.queue[category][element].events == 0) then
+    self.sc_logger:debug("[sc_flush:flush_queue]: queue with category: " .. tostring(category) .. " and element: "
+      .. tostring(element) .. " won't be flushed because there is no event stored in it.")
     return true
   end
 
   local rem = self.params.reverse_element_mapping;
 
+  -- flush if events in the queue are too old or if the queue is full
   if (self.queue[category][element].flush_date > self.params.max_buffer_age)
-    or (self.queue[category][element].buffer_size > self.params.max_buffer_size) then
+    or (#self.queue[category][element].events > self.params.max_buffer_size) 
+  then
     self.sc_logger:debug("sc_queue:flush_queue: flushing all the " .. rem[category][element] .. " events")
     local retval = send_method(self.queue[category][element].events, rem[category][element])
   else
@@ -165,6 +188,9 @@ function ScFlush:flush_queue(send_method, category, element)
   return retval
 end
 
+--- reset_queue: put a queue back to its initial state after flushing its events
+-- @param category (number) the category related to the queue
+-- @param element (number) the element related to the queue
 function ScFlush:reset_queue(category, element)
   self.queue[category][element].flush_date = os.time()
   self.queue[category][element].events = {}
