ee358e6d64f1f65201e6e2ae5f0d6a2ab6cb2895
tanguyvda <tcharles@centreon.com>
2021-07-13 15:40:33 +0200

    improve logging and comments in metrics module

--- modules/centreon-stream-connectors-lib/sc_metrics.lua
+++ modules/centreon-stream-connectors-lib/sc_metrics.lua
@@ -23,6 +23,7 @@ local ScMetrics = {}
 function sc_metrics.new(event, params, common, broker, logger)
   self = {}
 
+  -- create a default logger if it is not provided
   self.sc_logger = logger
   if not self.sc_logger then 
     self.sc_logger = sc_logger.new()
@@ -35,6 +36,7 @@ function sc_metrics.new(event, params, common, broker, logger)
   local categories = self.params.bbdo.categories
   local elements = self.params.bbdo.elements
 
+  -- store metric validation functions inside a table linked to category/element
   self.metric_validation = {
     [categories.neb] = {
       [elements.host.element] = function () return self:is_valid_host_metric_event() end,
@@ -47,19 +49,21 @@ function sc_metrics.new(event, params, common, broker, logger)
     }
   }
 
+  -- initiate metrics table 
   self.metrics = {}
-
+  -- initiate sc_event object
   self.sc_event = sc_event.new(event, self.params, self.sc_common, self.sc_logger, self.sc_broker)
 
   setmetatable(self, { __index = ScMetrics })
   return self
 end
 
+--- is_valid_bbdo_element: checks if the event category and element are valid according to parameters and bbdo protocol
+-- @return  true|false (boolean) depending on the validity of the event category and element
 function ScMetrics:is_valid_bbdo_element()
--- drop event if wrong category
+  -- initiate variables with shorter name
   local categories = self.params.bbdo.categories
   local elements = self.params.bbdo.elements
-
   local event_category = self.sc_event.event.category
   local event_element = self.sc_event.event.element
 
@@ -69,6 +73,8 @@ function ScMetrics:is_valid_bbdo_element()
   else
     -- drop event if accepted category is not supposed to be used for a metric stream connector
     if event_category ~= categories.neb and event_category ~= categories.bam then
+      self.sc_logger:warning("[sc_metrics:is_valid_bbdo_element] Configuration error. accepted categories from paramters are: "
+        .. tostring(self.params.accepted_categories) .. ". Only bam and neb can be used for metrics")
       return false
     else
       -- drop event if element is not accepted
@@ -82,136 +88,157 @@ function ScMetrics:is_valid_bbdo_element()
           and event_element ~= elements.service_status.element
           and event_element ~= elements.kpi_event.element
         then
+          self.sc_logger:warning("[sc_metrics:is_valid_bbdo_element] Configuration error. accepted elements from paramters are: "
+            .. tostring(self.params.accepted_elements) .. ". Only host, host_status, service, service_status and kpi_event can be used for metrics")
           return false
         end
       end
     end
-  end
-
-  function ScMetrics:is_valid_metric_event()
-    category = self.sc_event.event.category
-    element = self.sc_event.event.element
-    
-    return self.metric_validation[category][element]()
-  end
-
-  function ScMetrics:is_valid_host_metric_event()
-    -- return false if we can't get hostname or host id is nil
-    if not self.sc_event:is_valid_host() then
-      self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " hasn't been validated")
-      return false
-    end
-
-    -- return false if host is not monitored from an accepted poller
-    if not self.sc_event:is_valid_poller() then
-      self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " is not monitored from an accepted poller")
-      return false
-    end
-
-    -- return false if host has not an accepted severity
-    if not self.sc_event:is_valid_host_severity() then
-      self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " has not an accepted severity")
-      return false
-    end
-
-    -- return false if host is not in an accepted hostgroup
-    if not self.sc_event:is_valid_hostgroup() then
-      self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " is not in an accepted hostgroup")
-      return false
-    end
-
-    if not self:is_valid_perfdata(self.sc_event.event.perf_data) then
-      self.sc_logger:warning("[sc_metrics:is_vaild_host_metric_event]: host_id: "
-        .. tostring(self.sc_event.event.host_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
-      return false
-    end
-  end
-
-  function ScMetrics:is_valid_service_metric_event()
-    -- return false if we can't get hostname or host id is nil
-    if not self.sc_event:is_valid_host() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " hasn't been validated")
-      return false
-    end
-
-    -- return false if we can't get service description of service id is nil
-    if not self.sc_event:is_valid_service() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service with id: " .. tostring(self.sc_event.event.service_id) .. " hasn't been validated")
-      return false
-    end
-
-    -- return false if host is not monitored from an accepted poller
-    if not self.sc_event:is_valid_poller() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
-        .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. " is not monitored from an accepted poller")
-      return false
-    end
-
-    -- return false if host has not an accepted severity
-    if not self.sc_event:is_valid_host_severity() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
-        .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. ". Host has not an accepted severity")
-      return false
-    end
-
-    -- return false if service has not an accepted severity
-    if not self.sc_event:is_valid_service_severity() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
-        .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. ". Service has not an accepted severity")
-      return false
-    end
-
-    -- return false if host is not in an accepted hostgroup
-    if not self.sc_event:is_valid_hostgroup() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service_id: " .. tostring(self.sc_event.event.service_id) 
-        .. " is not in an accepted hostgroup. Host ID is: " .. tostring(self.sc_event.event.host_id))
-      return false
-    end
-
-    -- return false if service is not in an accepted servicegroup 
-    if not self.sc_event:is_valid_servicegroup() then
-      self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service_id: " .. tostring(self.sc_event.event.service_id) .. " is not in an accepted servicegroup")
-      return false
-    end
-
-    if not self:is_valid_perfdata(self.sc_event.event.perf_data) then
-      self.sc_logger:warning("[sc_metrics:is_vaild_service_metric_event]: service_id: "
-        .. tostring(self.sc_event.event.service_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
-      return false
-    end
 
     return true
   end
+end
 
-  function ScMetrics:is_valid_kpi_metric_event()
-    if not self:is_valid_perfdata(self.sc_event.event.perfdata) then
-      self.sc_logger:warning("[sc_metrics:is_vaild_kpi_metric_event]: kpi_id: "
-        .. tostring(self.sc_event.event.kpi_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
-      return false
-    end
+--- is_valid_metric_event: makes sure that the event is a valid event for metric usage
+-- @return true|false (boolean) depending on the validity of the metric event
+function ScMetrics:is_valid_metric_event()
+  category = self.sc_event.event.category
+  element = self.sc_event.event.element
+  
+  return self.metric_validation[category][element]()
+end
 
-    return true
+--- is_valid_host_metric_event: makes sure that the metric and the event from the host are valid according to the stream connector parameters
+-- @return true|false (boolean) depening on the validity of the event
+function ScMetrics:is_valid_host_metric_event()
+  -- return false if we can't get hostname or host id is nil
+  if not self.sc_event:is_valid_host() then
+    self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " hasn't been validated")
+    return false
   end
 
-  function ScMetrics:is_valid_perfdata(perfdata)
-    if not perfdata then
-      return false
-    end
+  -- return false if host is not monitored from an accepted poller
+  if not self.sc_event:is_valid_poller() then
+    self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " is not monitored from an accepted poller")
+    return false
+  end
 
-    local metrics_info, error = broker.parse_perfdata(perfdata, true)
+  -- return false if host has not an accepted severity
+  if not self.sc_event:is_valid_host_severity() then
+    self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " has not an accepted severity")
+    return false
+  end
 
-    if not metrics_info then
-      self.sc_logger:error("[sc_metrics:is_valid_perfdata]: couldn't parse perfdata. Error is: "
-        .. tostring(error) .. ". Perfdata string is: " .. tostring(perfdata))
-      return false
-    end
+  -- return false if host is not in an accepted hostgroup
+  if not self.sc_event:is_valid_hostgroup() then
+    self.sc_logger:warning("[sc_metrics:is_valid_host_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " is not in an accepted hostgroup")
+    return false
+  end
 
-    for metric_name, metric_data in pairs(metrics_info) do
-      self.metrics[metric_name] = metric_data
-      self.metrics[metric_name].name = metric_name
-    end
+  -- return false if there is no perfdata or they it can't be parsed
+  if not self:is_valid_perfdata(self.sc_event.event.perf_data) then
+    self.sc_logger:warning("[sc_metrics:is_vaild_host_metric_event]: host_id: "
+      .. tostring(self.sc_event.event.host_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
+    return false
+  end
 
-    return true
+  return true
+end
+
+--- is_valid_host_metric_event: makes sure that the metric and the event from the service are valid according to the stream connector parameters
+-- @return true|false (boolean) depening on the validity of the event
+function ScMetrics:is_valid_service_metric_event()
+  -- return false if we can't get hostname or host id is nil
+  if not self.sc_event:is_valid_host() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: host_id: " .. tostring(self.sc_event.event.host_id) .. " hasn't been validated")
+    return false
+  end
+
+  -- return false if we can't get service description of service id is nil
+  if not self.sc_event:is_valid_service() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service with id: " .. tostring(self.sc_event.event.service_id) .. " hasn't been validated")
+    return false
+  end
+
+  -- return false if host is not monitored from an accepted poller
+  if not self.sc_event:is_valid_poller() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
+      .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. " is not monitored from an accepted poller")
+    return false
+  end
+
+  -- return false if host has not an accepted severity
+  if not self.sc_event:is_valid_host_severity() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
+      .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. ". Host has not an accepted severity")
+    return false
+  end
+
+  -- return false if service has not an accepted severity
+  if not self.sc_event:is_valid_service_severity() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service id: " .. tostring(self.sc_event.event.service_id) 
+      .. ". host_id: " .. tostring(self.sc_event.event.host_id) .. ". Service has not an accepted severity")
+    return false
+  end
+
+  -- return false if host is not in an accepted hostgroup
+  if not self.sc_event:is_valid_hostgroup() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service_id: " .. tostring(self.sc_event.event.service_id) 
+      .. " is not in an accepted hostgroup. Host ID is: " .. tostring(self.sc_event.event.host_id))
+    return false
+  end
+
+  -- return false if service is not in an accepted servicegroup 
+  if not self.sc_event:is_valid_servicegroup() then
+    self.sc_logger:warning("[sc_metrics:is_valid_service_metric_event]: service_id: " .. tostring(self.sc_event.event.service_id) .. " is not in an accepted servicegroup")
+    return false
+  end
+
+  -- return false if there is no perfdata or they it can't be parsed
+  if not self:is_valid_perfdata(self.sc_event.event.perf_data) then
+    self.sc_logger:warning("[sc_metrics:is_vaild_service_metric_event]: service_id: "
+      .. tostring(self.sc_event.event.service_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
+    return false
+  end
+
+  return true
+end
+
+--- is_valid_host_metric_event: makes sure that the metric and the event from the KPI are valid according to the stream connector parameters
+-- @return true|false (boolean) depening on the validity of the event
+function ScMetrics:is_valid_kpi_metric_event()
+  if not self:is_valid_perfdata(self.sc_event.event.perfdata) then
+    self.sc_logger:warning("[sc_metrics:is_vaild_kpi_metric_event]: kpi_id: "
+      .. tostring(self.sc_event.event.kpi_id) .. " is not sending valid perfdata. Received perfdata: " .. tostring(self.sc_event.event.perf_data))
+    return false
+  end
+
+  return true
+end
+
+--- is_valid_perfdata: makes sure that the perfdata string is a valid one
+-- @param perfdata (string) a string that contains perfdata
+-- @return true|false (boolean) depending on the validity of the perfdata
+function ScMetrics:is_valid_perfdata(perfdata)
+  -- drop event if perfdata is nil or empty
+  if not perfdata or perfdata == "" then
+    return false
+  end
+
+  -- parse perfdata
+  local metrics_info, error = broker.parse_perfdata(perfdata, true)
+
+  -- drop event if parsing failed
+  if not metrics_info then
+    self.sc_logger:error("[sc_metrics:is_valid_perfdata]: couldn't parse perfdata. Error is: "
+      .. tostring(error) .. ". Perfdata string is: " .. tostring(perfdata))
+    return false
+  end
+
+  -- store data from parsed perfdata inside a metrics table
+  for metric_name, metric_data in pairs(metrics_info) do
+    self.metrics[metric_name] = metric_data
+    self.metrics[metric_name].name = metric_name
   end
 
   return true
