61b04a19b45f586e5a6097d353d7c0e7db39fb5f
tanguyvda <tcharles@centreon.com>
2023-01-05 17:15:44 +0100

    handle queue metadata in snow em events sc

--- centreon-certified/servicenow/servicenow-em-events-apiv2.lua
+++ centreon-certified/servicenow/servicenow-em-events-apiv2.lua
@@ -97,7 +97,7 @@ function EventQueue.new (params)
   }
 
   self.send_data_method = {
-    [1] = function (payload) return self:send_data(payload) end
+    [1] = function (payload, queue_metadata) return self:send_data(payload, queue_metadata) end
   }
 
   self.build_payload_method = {
@@ -412,7 +412,7 @@ end
 -- EventQueue:send_data, send data to external tool
 -- @return {boolean}
 --------------------------------------------------------------------------------
-function EventQueue:send_data(payload)
+function EventQueue:send_data(payload, queue_metadata)
   local authToken
   local counter = 0
 
