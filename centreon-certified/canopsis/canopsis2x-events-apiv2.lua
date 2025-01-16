71831f46502e5aae85c118078ff3b6fc0597f5a6
tanguyvda <tanguyvda@hotmail.fr>
2025-01-16 11:56:09 +0100

    fix canopsis duplicate data

--- centreon-certified/canopsis/canopsis2x-events-apiv2.lua
+++ centreon-certified/canopsis/canopsis2x-events-apiv2.lua
@@ -776,6 +776,15 @@ function EventQueue:postCanopsisAPI(self_metadata, route, data_to_send)
     self.sc_logger:notice("[postCanopsisAPI]: HTTP POST request successful: return code is "
     .. tostring(http_response_code))
     retval = true
+  elseif http_response_code == 400 and string.find(tostring(http_response_body), "ID already exists") then
+    self.sc_logger:notice("[EventQueue:send_data]: Tried to send duplicate data. It is going to be ignored. "
+      .. "return code is: " .. tostring(http_response_body) .. ". Message is: " .. tostring(http_response_body))
+    
+      if payload then
+        self.sc_logger:notice("[EventQueue:send_data]: sent payload was: " .. tostring(data_to_send))
+      end
+    
+    retval = true
   else
     self.sc_logger:error("[postCanopsisAPI]: HTTP POST request FAILED, return code is "
       .. tostring(http_response_code) .. ". Message is: " .. tostring(http_response_body))
