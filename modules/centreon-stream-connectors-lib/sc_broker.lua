ac4f0916aeb918d5704f9cd4276eaf5a797b5376
omercier <omercier@centreon.com>
2025-06-27 16:20:38 +0200

    enh sc_broker documentation

--- modules/centreon-stream-connectors-lib/sc_broker.lua
+++ modules/centreon-stream-connectors-lib/sc_broker.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/lua
 
---- Module with Centreon broker related methods for easier usage
+--- Module providing utility methods to interact with the Centreon broker.
+--- Facilitates retrieval of information about hosts, services, groups, severities, instances, BAs, and BVs.
 --- @module sc_broker
 
 local sc_broker = {}
@@ -23,33 +24,35 @@ function sc_broker.new(logger)
 end
 
 
---- Retrieve all information from a host
---- @param host_id number ID of the host
---- @return boolean false if host_id is not valid or no information was found in the broker cache
---- @return table all information from the host
+--- This function interacts with the broker cache to fetch all available information for a given host.
+--- If the host ID is invalid or no information is found, it logs a warning and returns `false`.
+--- @param host_id number ID of the host to retrieve information for
+--- @return boolean|table Returns `false` if the host ID is invalid or no information is found in the broker cache.
+--- Returns a table containing all information about the host if successful.
 function ScBroker:get_host_all_infos(host_id)
-  -- return because host_id isn't valid
+  -- Check if the host_id is valid (not nil or empty)
   if host_id == nil or host_id == "" then
     self.logger:warning("[sc_broker:get_host_all_infos]: host id is nil")
     return false
   end
-  
-  -- get host information from broker cache
+
+  -- Retrieve host information from the broker cache
   local host_info = broker_cache:get_host(host_id)
 
-  -- return false only if no host information were found in broker cache
+  -- Check if host information was found in the broker cache
   if not host_info then
-    self.logger:warning("[sc_broker:get_host_all_infos]: No host information found for host_id:  " .. tostring(host_id) .. ". Restarting centengine should fix this.")  
+    self.logger:warning("[sc_broker:get_host_all_infos]: No host information found for host_id:  " .. tostring(host_id) .. ". Restarting centengine should fix this.")
     return false
   end
 
   return host_info
 end
 
---- Retrieve information from a service
---- @param host_id (number) ID of the host
---- @param service_id (number)
---- @return (boolean|table) Table of all the information from the service. Returns false if host id or service id aren't valid.
+--- Retrieves all available information for a specific service from the broker cache.
+--- Logs a warning and returns `false` if the host or service ID is invalid or if no information is found.
+--- @param host_id number The ID of the host associated with the service.
+--- @param service_id number The ID of the service to retrieve information for.
+--- @return table|boolean Returns a table with all service information if successful, or `false` if the IDs are invalid or no data is found.
 function ScBroker:get_service_all_infos(host_id, service_id)
   -- return because host_id or service_id isn't valid
   if host_id == nil or host_id == "" or service_id == nil or service_id == "" then
@@ -70,44 +73,49 @@ function ScBroker:get_service_all_infos(host_id, service_id)
   return service_info
 end
 
---- Retrieve the the desired host informations
---- @param host_id (number) ID of the host
---- @param info (string|table) Name of the wanted host parameter or a table of all wanted host parameters
---- @return (boolean|table) Table of all wanted host params if input param is a table. The single parameter if input param is a string. Returns false if host_id is nil or empty.
+--- Retrieves the desired information about a host from the broker cache.
+--- If the host ID is invalid or empty, logs a warning and returns `false`.
+--- If no specific parameter is requested, returns a table with only the host ID.
+--- If the host is not found in the cache, returns a table with only the host ID.
+--- If a string is provided as `info`, returns the corresponding parameter value if it exists.
+--- If a table is provided as `info`, returns a table with the requested parameters.
+--- @param host_id number ID of the host.
+--- @param info string|table Name of the desired host parameter (string) or a table of all desired host parameters.
+--- @return boolean|table Table of all requested host parameters if `info` is a table, the single parameter if `info` is a string, or `false` if `host_id` is nil or empty.
 function ScBroker:get_host_infos(host_id, info)
-  -- return because host_id isn't valid
+  -- Return false if host_id is not valid
   if host_id == nil or host_id == "" then
     self.logger:warning("[sc_broker:get_host_infos]: host id is nil")
     return false
   end
-  
-  -- prepare return table with host information
+
+  -- Prepare return table with host_id
   local host = {
     host_id = host_id
   }
 
-  -- return host_id only if no specific param is asked
+  -- Return host_id only if no specific parameter is requested
   if info == nil then
     return host
   end
 
-  -- get host information from broker cache
+  -- Get host information from broker cache
   local host_info = broker_cache:get_host(host_id)
 
-  -- return host_id only if no host information were found in broker cache
+  -- Return host_id only if no host information was found in broker cache
   if not host_info then
-    self.logger:warning("[sc_broker:get_host_infos]: No host information found for host_id:  " .. tostring(host_id) .. ". Restarting centengine should fix this.")  
+    self.logger:warning("[sc_broker:get_host_infos]: No host information found for host_id:  " .. tostring(host_id) .. ". Restarting centengine should fix this.")
     return host
   end
 
-  -- get the desired param and return the information
+  -- Get the desired parameter and return the information
   if type(info) == "string" then
     if host_info[info] then
       return host_info[info]
     end
   end
 
-  -- get all the desired params and return the information
+  -- Get all the desired parameters and return the information
   if type(info) == "table" then
     for _, param in ipairs(info) do
       if host_info[param] then
@@ -120,46 +128,49 @@ function ScBroker:get_host_infos(host_id, info)
 end
 
 --- Retrieve the the desired service informations
---- @param host_id (number) ID of the host
---- @param service_id (number) ID of the service
---- @param info (string|table) the name of the wanted host parameter or a table of all wanted service parameters
---- @return (boolean|table) Table of all wanted service params if input param is a table. A single parameter if input param is a string. Returns false if host_id and/or service_id are nil or empty
+--- This function fetches specific service information from the broker cache based on the provided host ID, service ID, and requested parameters.
+--- Logs warnings if the host ID or service ID is invalid or if no service information is found in the broker cache.
+--- @param host_id number ID of the host
+--- @param service_id number ID of the service
+--- @param info string|table The name of the desired service parameter (string) or a table of all desired service parameters
+--- @return boolean|table Returns a table containing the requested service parameters if successful.
+--- Returns a single parameter if `info` is a string. Returns `false` if `host_id` or `service_id` are invalid or empty.
 function ScBroker:get_service_infos(host_id, service_id, info)
-  -- return because host_id or service_id isn't valid
+  -- Return false if host_id or service_id is invalid
   if host_id == nil or host_id == "" or service_id == nil or service_id == "" then
     self.logger:warning("[sc_broker:get_service_infos]: host id or service id is invalid")
     return false
   end
-  
-  -- prepare return table with service information
+
+  -- Prepare a return table with basic service information
   local service = {
     host_id = host_id,
     service_id = service_id
   }
 
-  -- return host_id and service_id only if no specific param is asked
+  -- Return basic service information if no specific parameter is requested
   if info == nil then
     return service
   end
 
-  -- get service information from broker cache
+  -- Retrieve service information from the broker cache
   local service_info = broker_cache:get_service(host_id, service_id)
 
-  -- return host_id and service_id only if no host information were found in broker cache
+  -- Return basic service information if no data is found in the broker cache
   if not service_info then
-    self.logger:warning("[sc_broker:get_service_infos]: No service information found for host_id:  " .. tostring(host_id) .. " and service_id: " .. tostring(service_id) 
-      .. ". Restarting centengine should fix this.")  
+    self.logger:warning("[sc_broker:get_service_infos]: No service information found for host_id:  " .. tostring(host_id) .. " and service_id: " .. tostring(service_id)
+      .. ". Restarting centengine should fix this.")
     return service
   end
 
-  -- get the desired param and return the information
+  -- Retrieve and return the requested parameter if `info` is a string
   if type(info) == "string" then
     if service_info[info] then
       return service_info[info]
     end
   end
 
-  -- get all the desired params and return the information
+  -- Retrieve and return all requested parameters if `info` is a table
   if type(info) == "table" then
     for _, param in ipairs(info) do
       if service_info[param] then
@@ -172,8 +183,11 @@ function ScBroker:get_service_infos(host_id, service_id, info)
 end
 
 --- Retrieve hostgroups from host_id
---- @param host_id (number) ID of the host
---- @return (boolean|table) Table of all hostgroups of the host or false if host id is invalid or no hostgroup found
+--- This function fetches all hostgroups associated with a given host ID from the broker cache.
+--- Logs a warning if the host ID is invalid or empty, and returns `false` if no hostgroups are found.
+--- @param host_id number ID of the host
+--- @return boolean|table Returns a table containing all hostgroups of the host if successful.
+--- Returns `false` if the host ID is invalid or no hostgroups are found.
 function ScBroker:get_hostgroups(host_id)
   -- return false if host id is invalid
   if host_id == nil or host_id == "" then 
@@ -193,8 +207,11 @@ function ScBroker:get_hostgroups(host_id)
 end
 
 --- Retrieve hostgroup alias from hostgroup_id
+--- This function fetches the alias of a specific hostgroup based on its ID from the broker cache.
+--- Logs a warning if the hostgroup ID is invalid or empty, and returns `false` if no alias is found.
 --- @param hostgroup_id number ID of the host group
---- @return (boolean|string)  Hostgroup alias or false if hostgroup ID is invalid
+--- @return boolean|string Returns the alias of the hostgroup if successful.
+--- Returns `false` if the hostgroup ID is invalid or no alias is found.
 function ScBroker:get_hostgroup_alias(hostgroup_id)
   -- return false if host id is invalid
   if hostgroup_id == nil or hostgroup_id == "" then
@@ -214,9 +231,12 @@ function ScBroker:get_hostgroup_alias(hostgroup_id)
 end
 
 --- Retrieve servicegroups from service_id
---- @param host_id (number) ID of the host
---- @param service_id (number) ID of the service
---- @return (boolean|table) Table of all servicegroups of the service or false if host_id or service_id is invalid or no information are found in the broker_cache
+--- This function fetches all servicegroups associated with a given service ID and host ID from the broker cache.
+--- Logs a warning if the host ID or service ID is invalid or empty, and returns `false` if no servicegroups are found.
+--- @param host_id number ID of the host
+--- @param service_id number ID of the service
+--- @return boolean|table Returns a table containing all servicegroups of the service if successful.
+--- Returns `false` if the host ID or service ID is invalid or no servicegroups are found.
 function ScBroker:get_servicegroups(host_id, service_id)
   -- return false if service id is invalid
   if host_id == nil or host_id == "" or service_id == nil or service_id == "" then 
@@ -236,9 +256,13 @@ function ScBroker:get_servicegroups(host_id, service_id)
 end
 
 --- Retrieve severity from host or service
---- @param host_id (number) ID of the host
---- @param service_id (number) OPTIONAL: ID of the service (do not use for a host)
---- @return (boolean|table) Severity of a host/service or false if host_id is invalid or no information are found in the broker_cache
+--- This function fetches the severity level of a host or service from the broker cache.
+--- Logs a warning if the host ID is invalid or empty, and returns `false` if no severity is found.
+--- If a service ID is provided, it retrieves the severity for the service; otherwise, it retrieves the severity for the host.
+--- @param host_id number ID of the host
+--- @param service_id number OPTIONAL: ID of the service (do not use for a host)
+--- @return boolean|table Returns the severity of the host or service if successful.
+--- Returns `false` if the host ID is invalid or no severity is found.
 function ScBroker:get_severity(host_id, service_id)
   -- return false if host id is invalid
   if host_id == nil or host_id == "" then 
@@ -274,9 +298,12 @@ function ScBroker:get_severity(host_id, service_id)
   return severity
 end
 
---- Retrieve poller from instance_id
---- @param host_id (number) ID of the host
---- @return (boolean|table) Name of the poller/instance or false if host_id is invalid or no information are found in the broker_cache
+--- Retrieve poller information from instance ID
+--- This function fetches the name of the poller/instance associated with a given instance ID from the broker cache.
+--- Logs a warning if the instance ID is invalid or empty, and returns `false` if no information is found.
+--- @param instance_id number ID of the instance
+--- @return boolean|string Returns the name of the poller/instance if successful.
+--- Returns `false` if the instance ID is invalid or no information is found in the broker cache.
 function ScBroker:get_instance(instance_id)
   -- return false if instance_id is invalid
   if instance_id == nil or instance_id == "" then
@@ -296,9 +323,12 @@ function ScBroker:get_instance(instance_id)
   return name
 end
 
---- Retrieve BA name and description from ba id
---- @param ba_id (number) ID of the BA
---- @return (boolean|table) Name and description of all the BA or false if ba_id is invalid or no information are found in the broker_cache
+--- Retrieve BA information from BA ID
+--- This function fetches the name and description of a specific Business Activity (BA) based on its ID from the broker cache.
+--- Logs a warning if the BA ID is invalid or empty, and returns `false` if no information is found.
+--- @param ba_id number ID of the Business Activity (BA)
+--- @return boolean|table Returns a table containing the name and description of the BA if successful.
+--- Returns `false` if the BA ID is invalid or no information is found in the broker cache.
 function ScBroker:get_ba_infos(ba_id)
   -- return false if ba_id is invalid
   if ba_id == nil or ba_id == "" then 
@@ -318,9 +348,12 @@ function ScBroker:get_ba_infos(ba_id)
   return ba_info
 end
 
---- Retrieve bv name and description from ba_id
---- @param ba_id (number)
---- @return (boolean|table) Name and description of all the bvs or false if ba_id is invalid or no information are found in the broker_cache
+--- Retrieve Business View (BV) names and descriptions associated with a given Business Activity (BA) ID.
+--- This function interacts with the broker cache to fetch all BV information linked to the specified BA ID.
+--- Logs warnings if the BA ID is invalid or empty, or if no BV information is found.
+--- @param ba_id number ID of the Business Activity (BA).
+--- @return boolean|table Returns a table containing names and descriptions of all BVs if successful.
+--- Returns `false` if the BA ID is invalid or no BV information is found in the broker cache.
 function ScBroker:get_bvs_infos(ba_id)
   -- return false if ba_id is invalid
   if ba_id == nil or ba_id == "" then 
